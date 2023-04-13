e2d249f153cc5b108204536fcc57477354da5ef2
kram22000 <95735766+kram22000@users.noreply.github.com>
2023-04-13 16:08:36 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // Send a POST request to your backend API with the user's email and password
+    // Handle any errors and redirect the user if the login is successful
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {error && <div>{error}</div>}
+    </div>
+  );
+}
+
+export default LoginPage;
