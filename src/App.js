0b5edb43e2fdc4cc4e572df8b34a3c52be7b53ab
Konrad Pawlikowski <103622147+CloudlessStudio@users.noreply.github.com>
2023-04-15 13:40:59 -0500

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
 import React, { useState } from 'react';
+import SignupPage from './components/SignupPage';
 
 function LoginPage() {
-  const [user, setUserID] = useState('');
+  const [userid, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [showSignup, setShowSignup] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
-
-    // Send a POST request to your backend API with the user's email and password
-    // Handle any errors and redirect the user if the login is successful
+  
+    fetch(`http://localhost:2000/authenticate?name=${userid}&password=${password}`)
+      .then(response => {
+        if (response.status === 401) {
+          throw new Error("Incorrect password");
+        }
+        if (response.status === 404) {
+          throw new Error("User not found");
+        }
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('User Found:', data);
+        setShowSignup(false);
+      })
+      .catch(error => {
+        console.log(error.message);
+        setError(error.message);
+      });
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <div className='main'>
+      {showSignup ? (
+        <SignupPage setShowSignup={setShowSignup} />
+      ) : (
         <div>
-          <label>UserID:</label>
-          <input type="user" value={user} onChange={(e) => setUserID(e.target.value)} />
+          <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input type="text" value={userid} onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <button onClick={() => setShowSignup(true)}>Sign Up</button>
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      )}
       {error && <div>{error}</div>}
     </div>
   );
